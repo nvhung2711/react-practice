909fd54a3eaf330f23f8e63827d577411c432b04
nvhung2711 <nvhung2711@gmail.com>
2025-11-01 14:44:12 +1100

    Normal CSS file styling and in-line styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import './App.css';
 
 const Card = ({ title }) => {
-    return <h3>{title}</h3>;
+    return (
+        <div
+            style={{
+                border: '1px solid #4b5362',
+                padding: '20px',
+                margin: '10px',
+                backgroundColor: '#31363f',
+                borderRadius: '10px',
+                minHeight: '100px',
+            }}
+        >
+            <h2>{title}</h2>
+        </div>
+    );
 };
 
 const App = () => {
     return (
-        <>
-            <h2>Functional Arrow Component</h2>;
-            <Card title="Star Wars" />
+        <div className="card-container">
+            <Card
+                title="Star Wars"
+                rating={5}
+                isCool={true}
+                actors={[{ name: 'Actors' }]}
+            />
             <Card title="Avatar" />
             <Card title="The Lion King" />
-        </>
+        </div>
     );
 };
 
